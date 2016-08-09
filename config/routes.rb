b9ae341a9cd969796a6be0150c4d2426528cea61
Maddie Healy <maddiehealy@gmail.com>
2016-08-09 10:32:19 -0400

    Changed Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+	root 'topics#index'
   resources :categories
   resources :applications
+  resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
